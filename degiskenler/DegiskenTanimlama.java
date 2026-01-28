3e4be7e8393c9668fe5fda369523a38c894ab0ca
BEYZANUR AKGÜÇ <153449879+beyzaakgucc@users.noreply.github.com>
2026-01-28 16:43:43 +0300

    Update DegiskenTanimlama.java

--- degiskenler/DegiskenTanimlama.java
+++ degiskenler/DegiskenTanimlama.java
@@ -1,12 +1,18 @@
 //Değişken = bilgisayarda bir bilgiyi saklamak için verdiğimiz isim
 public class OgrenciProfil {
     public static void main(String[] args) {
+        
         int yas = 21;
+        int yas = 20.5;   // HATA int tam sayılar içindir.
+        double yas=20.5; //DOĞRU✅
+
         double notOrtalamasi = 3;
+        
         String isim = "Beyzanur";
+        String isim = Beyzanur; // HATA!! String türündeki değerler Java’da metin olduğu için çift tırnak içinde yazılmalıdır.
         boolean mezunMu = false;
 
-        System.out.println(isim);
+        System.out.println(isim); //YAZDIRMA İŞLEMLLERİ
         System.out.println(yas);
         System.out.println(notOrtalamasi);
         System.out.println(mezunMu);
