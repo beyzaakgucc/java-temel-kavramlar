554ac657d6b76942eaade6aa70f4ffbabc252c39
BEYZANUR AKGÜÇ <153449879+beyzaakgucc@users.noreply.github.com>
2026-01-28 16:39:52 +0300

    Create DegiskenTanimlama.java

---
+++ degiskenler/DegiskenTanimlama.java
@@ -0,0 +1,14 @@
+//Değişken = bilgisayarda bir bilgiyi saklamak için verdiğimiz isim
+public class OgrenciProfil {
+    public static void main(String[] args) {
+        int yas = 21;
+        double notOrtalamasi = 3;
+        String isim = "Beyzanur";
+        boolean mezunMu = false;
+
+        System.out.println(isim);
+        System.out.println(yas);
+        System.out.println(notOrtalamasi);
+        System.out.println(mezunMu);
+    }
+}
