a5efa0f2884f01b3e3db0c32cb55476f5f89537c
BEYZANUR AKGÜÇ <153449879+beyzaakgucc@users.noreply.github.com>
2026-01-28 16:54:53 +0300

    Create ReferenceOrnek.java

---
+++ degiskenler/veri-tipleri/veri-tipleri/ReferenceOrnek.java
@@ -0,0 +1,10 @@
+public class ReferenceOrnek {
+    public static void main(String[] args) {
+
+        String isim = "Beyza";
+        String bolum = "Yazılım Muhendisligi";
+
+        System.out.println(isim);
+        System.out.println(bolum);
+    }
+}
