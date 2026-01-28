35e314989ab0fcce85f49f206c2891730740245c
BEYZANUR AKGÜÇ <153449879+beyzaakgucc@users.noreply.github.com>
2026-01-28 16:51:52 +0300

    Create PrimitiveOrnek.java

---
+++ degiskenler/veri-tipleri/veri-tipleri/PrimitiveOrnek.java
@@ -0,0 +1,14 @@
+public class PrimitiveOrnek {
+    public static void main(String[] args) {
+
+        int yas = 21;
+        double ortalama = 3;
+        boolean ogrenciMi = true;
+        char harf = 'B';
+
+        System.out.println(yas);
+        System.out.println(ortalama);
+        System.out.println(ogrenciMi);
+        System.out.println(harf);
+    }
+}
